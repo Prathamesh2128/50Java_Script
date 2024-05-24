c90e22a88e90b5e0f9f8156c613b681967fa9f51
Prathamesh Shinde <prathamesh.shinde@TECKNOTROVE.LOCAL>
2024-05-24 09:35:34 +0530

    Veriables and constant

---
+++ 02_VarialbleConstant/varialbles.js
@@ -0,0 +1,15 @@
+const accountId = 212810;
+let accountEmail = "pathushinde123@gmail.com";
+var accountPass = "123456789";
+accountCity = "mumbai";
+let accountState;
+
+
+//accountId = 12;
+accountEmail = "shinde@gmail.com";
+
+//console.log(accountId);
+//console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPass,accountState]);
+
